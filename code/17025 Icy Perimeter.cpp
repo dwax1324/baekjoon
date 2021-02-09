d05a3a91cb9aecaae621aec89b9ec09aa73baed4
dwax1324 <59137236+dwax1324@users.noreply.github.com>
2021-02-09 13:36:22 +0900

    2021_02_09

---
+++ code/17025 Icy Perimeter.cpp
@@ -0,0 +1,56 @@
+﻿// 2021-02-09
+// bfs , size and perimeter of components
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef pair<int, int> pii;
+int N;
+
+char mapp[1001][1001];
+bool vis[1001][1001];
+
+pii bfs(int a, int b) {
+	queue<pii> q;
+	vis[a][b] = 1;
+	q.push({ a,b });
+	int peri = 0, area = 0;
+	while (q.size()) {
+		int r = q.front().first, c = q.front().second; q.pop();
+		area++;
+		for (int d = 0; d < 4; d++) {
+			int nr = r + "2011"[d] - '1';
+			int nc = c + "1102"[d] - '1';
+			if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
+				peri++; continue;
+			}
+			if (mapp[nr][nc] == '.') peri++;
+		}
+		for (int d = 0; d < 4; d++) {
+			int nr = r + "2011"[d] - '1';
+			int nc = c + "1102"[d] - '1';
+			if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
+			if (vis[nr][nc] || mapp[nr][nc] != '#') continue;
+			q.push({ nr,nc });
+			vis[nr][nc] = 1;
+		}
+	}
+	return { area,peri };
+}
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> mapp[i][j];
+	vector<pii> ans;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (mapp[i][j] == '#' && !vis[i][j]) {
+				ans.push_back(bfs(i, j));
+			}
+	sort(ans.begin(), ans.end(), [](pii a, pii b) {
+		if (a.first == b.first) return a.second < b.second;
+		return a.first > b.first;
+		});
+	cout << ans[0].first << " " << ans[0].second;
+}
---
+++ code/17836 공주님을 구해라! .cpp
@@ -0,0 +1,49 @@
+﻿// bfs state management
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef tuple<int, int, int> tii;
+int N, M, T;
+int mapp[101][101];
+bool vis[101][101][2];
+int main() {
+	cin >> N >> M >> T;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			cin >> mapp[i][j];
+	queue<tii> q;
+	q.push({ 0, 0 ,0 });
+	vis[0][0][0] = 1;
+	int ans;
+	bool found = 0;
+	for (ans = 0; q.size(); ans++) {
+		int qs = q.size();
+		for (int i = 0; i < qs; i++) {
+			int r, c, gram;
+			tie(r, c, gram) = q.front();
+			q.pop();
+			if (r == N - 1 && c == M - 1) {
+				found = true;
+				goto bof;
+			}
+			for (int d = 0; d < 4; d++) {
+				int nr = r + "2011"[d] - '1';
+				int nc = c + "1102"[d] - '1';
+				if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+				if (vis[nr][nc][gram] || (gram == 0 && mapp[nr][nc] == 1)) continue;
+				if (mapp[nr][nc] == 2 && gram == 0) {
+					q.push({ nr,nc,1 });
+					vis[nr][nc][1] = 1;
+				}
+				else {
+					q.push({ nr,nc,gram });
+					vis[nr][nc][gram] = 1;
+				}
+			}
+
+		}
+	}
+bof:
+	if (ans <= T && found) cout << ans;
+	else cout << "Fail";
+}
