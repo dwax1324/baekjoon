3a6daf8a66dbb304af8a4002989c081dcffc831d
dwax1324 <59137236+dwax1324@users.noreply.github.com>
2021-03-29 20:51:30 +0900

    2021_03_29

---
+++ code/21185 Some Sum.cpp
@@ -0,0 +1,20 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	bool odd = false, even = false;
+	for (int i = 1; i <= 100; i++) {
+		int SUM = 0;
+		for (int j = i; j < i + n; j++) {
+			SUM += j;
+		}
+		if (SUM % 2) odd = true;
+		else even = true;
+	}
+	if (odd && even) cout << "Either";
+	else if (odd) cout << "Odd";
+	else cout << "Even";
+
+}
